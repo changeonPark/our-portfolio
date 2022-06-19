dbdc1e59fa12f503d737d80dc162c6037cad763d
parkchangeon <cgp@altava.com>
2022-06-20 00:27:30 +0900

    projects page 1차 완성

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,13 +2,13 @@ import { Layout } from "base/components"
 
 import { NextPage } from "next"
 import Head from "next/head"
+import { Item } from "projects/components"
 
 type Props = {
   projects: any[]
 }
 
-const Projects: NextPage<Props> = ({projects}) => {
-  console.log(projects)
+const Projects: NextPage<Props> = ({ projects }) => {
   return (
     <Layout>
       <Head>
@@ -17,13 +17,15 @@ const Projects: NextPage<Props> = ({projects}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>총 프로젝트: {projects.length}</h1>
+      <h1 className="text-4xl font-bold mx-5 2xl:mx-auto">
+        총 프로젝트: <span className="text-orange-400">{projects.length}</span>
+      </h1>
 
-      {
-        projects.map((item, key) => (
-          <h1 key={key}>{item.properties["이름"].title[0].plain_text}</h1>
-        ))
-      }
+      <div className="mx-5 py-10 2xl:mx-auto max-w-7xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+        {projects.map((item, key) => (
+          <Item data={item} key={key} />
+        ))}
+      </div>
     </Layout>
   )
 }
@@ -39,14 +41,15 @@ export async function getStaticProps() {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
     },
-    body: JSON.stringify({ 
+    body: JSON.stringify({
       sorts: [
         {
-          "property": "이름",
-          "direction": "ascending"
-        }
+          property: "이름",
+          direction: "ascending",
+        },
       ],
-      page_size: 100 }),
+      page_size: 100,
+    }),
   }
 
   const res = await fetch(
@@ -55,11 +58,13 @@ export async function getStaticProps() {
   )
 
   const projects = await res.json()
-  
-  const projectNames = projects.results.map((item: any) => item.properties["이름"].title[0].plain_text)
+
+  const projectNames = projects.results.map(
+    (item: any) => item.properties["이름"].title[0].plain_text
+  )
   console.log(projectNames)
 
   return {
-    props: {projects: projects.results}, // will be passed to the page component as props
+    props: { projects: projects.results }, // will be passed to the page component as props
   }
 }
---
+++ src/projects/components/Item.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link"
+import Image from "next/image"
+
+type Props = {
+  data: any
+}
+
+const Item = ({ data }: Props) => {
+  const title: string = data.properties["이름"].title[0].plain_text
+  const github: string = data.properties["깃허브"].url
+  const description: string =
+    data.properties["간단 설명"].rich_text[0].plain_text
+  const src = data.cover.external?.url || data.cover.file?.url
+  const tags: {
+    id: string
+    name: string
+    color: string
+  }[] = data.properties["태그"].multi_select
+  const start = data.properties['날짜'].date.start
+  const end = data.properties['날짜'].date.end
+
+
+  return (
+    <div className="projects-card">
+      <Image
+        className="rounded-t-xl"
+        src={src}
+        width="100%"
+        height="100%"
+        alt="cover_image"
+        layout="responsive"
+        objectFit="fill"
+        quality={100}
+      />
+      <div className="flex flex-col gap-4 px-4 my-4">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <Link href={github}>
+          <a target="_blank">GitHub</a>
+        </Link>
+        <h3>작업 기간: <span>{start} ~ {end}</span></h3>
+        <div className="flex items-center gap-3">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className={`rounded-md px-2 py-1 ${tag.color === 'yellow' ? 'bg-yellow-500' : 'bg-red-700'}`}
+            >
+              {tag.name}
+            </span>
+          ))}
+        </div>
+        <h3>{description}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Item
