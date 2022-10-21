bf7bb89e841c4f28079676f03ebfc3214a3c4b65
changeonPark <qkrcksrjs@gmail.com>
2022-10-21 18:34:55 +0900

    chore: init-storybook
    스토리 북 설치 및 간단 실습

--- src/base/components/index.ts
+++ src/base/components/index.ts
@@ -1,2 +1,3 @@
-export {default as Layout} from "./Layout"
-export {default as ModeToggle} from "./ModeToggle"
+export { default as Layout } from "./Layout";
+export { default as ModeToggle } from "./ModeToggle";
+export { default as Header } from "./Header";
