826454cd7ed66b3941f828a9d630b6d621f5437e
changeonPark <qkrcksrjs@gmail.com>
2022-11-04 21:57:48 +0900

    :recycle: fetch 클래스 함수로 재작성

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 import { Layout } from "base/components";
 import { ProjectResult, RootObject } from "interface/project";
-import projectAPI from "libs/api/project";
+import { getProjects, getProjectInfo } from "libs/api/project";
 import { GetStaticPropsResult, NextPage } from "next";
 import { Project } from "projects/components";
 
@@ -23,12 +23,12 @@ const Projects: NextPage<Props> = ({ data }) => {
 export default Projects;
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
-  const projectList = await projectAPI.getProject();
+  const projectList = await getProjects();
 
   let result: ProjectResult = {};
 
   for (const item of projectList) {
-    const projectInfo: RootObject[] = await projectAPI.getProjectInfo(item.id);
+    const projectInfo: RootObject[] = await getProjectInfo(item.id);
     result[item.id] = {
       projectName: item.title[0].plain_text,
       data: projectInfo,
--- src/libs/api/project.ts
+++ src/libs/api/project.ts
@@ -51,4 +51,50 @@ class ProjectAPI {
   }
 }
 
-export default new ProjectAPI();
+export const classAPI = new ProjectAPI();
+
+const defaultOption = {
+  headers: {
+    accept: "application/json",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json",
+    authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+  },
+};
+
+const fetcher = (option: Object) => {
+  try {
+    return axios.request(option).then(res => res.data.results);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getProjects = () => {
+  const option = {
+    ...defaultOption,
+    method: "POST",
+    url: "https://api.notion.com/v1/search",
+    data: { filter: { value: "database", property: "object" }, page_size: 20 },
+  };
+  return fetcher(option);
+};
+
+export const getProjectInfo = (projectId: string) => {
+  const option = {
+    ...defaultOption,
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${projectId}/query`,
+    data: {
+      sorts: [
+        {
+          property: "date",
+          direction: "descending",
+        },
+      ],
+      page_size: 100,
+    },
+  };
+  return fetcher(option);
+};
