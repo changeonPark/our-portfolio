7202a5a798ea40dbc1a211a48f16e6eec29c09fb
changeonPark <qkrcksrjs@gmail.com>
2022-11-12 22:44:22 +0900

    !BREAK CHANGE: notion API change name
    tag에서 skill로 변경
    이에 따라 type 및 Item.tsx에서 property name 변경

--- src/home/components/Hero.tsx
+++ src/home/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
   return (
     <section className="text-light dark:text-dark flex flex-col px-3">
-      <article className="flex flex-col gap-2 md:gap-4">
+      <article className="flex flex-col gap-3 md:gap-6">
         <p className="text-base md:text-2xl">방문해 주셔서 감사합니다!</p>
         <h1 className="text-2xl md:text-4xl lg:text-6xl font-accent flex gap-2">
           <span>I`am</span>
@@ -21,6 +21,11 @@ const Hero = () => {
             ))}
           </div>
         </h1>
+        <div className="text-light/80 dark:text-dark/80 text-sm md:text-base flex flex-col gap-1">
+          <p>유지 보수에 용이하고, 재사용성이 높은 코드 작성을 위해 노력합니다</p>
+          <p>어려운 문제를 서로 공유하고 함께 해결하는 것을 좋아합니다</p>
+          <p>문제를 다양한 방면으로 도전해보는 것을 좋아합니다</p>
+        </div>
       </article>
 
       {/* <article>
